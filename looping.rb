ac6bff9fb3de15b9776870c0c3d7578268b52ee6
Robert Allende <allende.rob@gmail.com>
2023-01-09 21:47:42 -0600

    commit tree 897f6692e514bf4be9b1d04f7597723a902a6397

--- looping.rb
+++ looping.rb
@@ -1,6 +1,14 @@
+require 'pry'
+
 def happy_new_year
-  # your code here
+  counter = 10
+  until counter == 0
+    puts counter
+    counter = counter - 1
+  end 
+  puts "Happy New Year!"
 end
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -16,9 +24,34 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+  i = 1
+  while i < 101
+    if i % 3 == 0 && i % 5 == 0
+      puts "FizzBuzz"
+    elsif i % 3 == 0
+      puts "Fizz"
+    elsif i % 5 == 0
+      puts "Buzz"
+    else
+      puts "#{i}"
+    end
+    i += 1
+  end
 end
 
 def reverse_string(str)
-  # your code here
+  reps = str.length
+  string = ""
+  reps.times do |i| 
+    string = str[i] + string 
+  end
+  string
 end
+    
+    # binding.pry
+  # i = 0
+  # while i < str.length
+  #   reversed_str = "#{str[i]}" + "#{reversed_str}"
+  #   i += 1
+  #   binding.pry
+  # end
